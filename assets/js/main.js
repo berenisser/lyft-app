b3f7cc9ad8870f002ffd8839ed0eef7adbf52719
VaneGarciaYara <vanegarciay@gmail.com>
2017-07-01 17:52:34 -0400

    maqueteado completo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,8 @@
  $(document).ready(function() {
-    $('select').material_select();
+    $('.modal').modal({
+        dismissible: true, // Modal can be dismissed by clicking outside of the modal
+        startingTop: '4%', // Starting top style attribute
+        endingTop: '100%' // Ending top style attribute
+    });
+
 });
