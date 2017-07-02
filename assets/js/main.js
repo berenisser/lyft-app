12f37a65dc5c8631a80e2bbf4a2a28c3da42eba9
VaneGarciaYara <vanegarciay@gmail.com>
2017-07-01 21:33:41 -0400

    actualizacion

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,6 @@
         startingTop: '4%', // Starting top style attribute
         endingTop: '100%' // Ending top style attribute
     });
-    $('.tooltipped').tooltip();
 
     $("input[name=phone]").keyup(function(event) {
         var prefijo = $("input[name=prefix-phone]").val();
@@ -13,11 +12,27 @@
             $(this).css('border-color','#FF0000');
             $("#phone_button").addClass('disabled');
             return false;
-        }
-        else {
+        } else {
             $(this).css('border-color','#0aa827');
             $("#phone_button").removeClass('disabled');
-            $('.tooltipped').tooltip('remove');
+        }
+    });
+
+    $("input[name=phone]").change(function(event) {
+        var prefijo = $("input[name=prefix-phone]").val();
+
+        if (prefijo == "+56" && ($("input[name=phone]").val().length != 9 || isNaN($("input[name=phone]").val()))) {
+            Materialize.toast('Debes ingresar un número válido', 2000)
+            return false;
+        }
+    });
+
+    $("#listado-paises a").click(function(event) {
+        var ids = $(this).attr("id");
+        console.log(ids);
+        if( ids == $("#mexico")){
+            $( "input[name=prefix-phone]" ).replaceWith( "OK" );
+            console.log("si cumpli");
         }
     });
 });
