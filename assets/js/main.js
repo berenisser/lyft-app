b8191af896db90ca8a9b3bf17d2bb756e4bab402
VaneGarciaYara <vanegarciay@gmail.com>
2017-07-01 23:17:05 -0400

    mexico y peru ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,7 +20,7 @@
         }
     });
 
-    $("#listado-paises a").click(function(event) {
+    $("#listado-paises").on("click", "a", function(event) {
         var ids = $(this).attr("id");
         if( ids == "mexico"){
             $( "input[name=prefix-phone]" ).val( "+52" );
@@ -31,12 +31,33 @@
             if (prefijo == "+52" && ($(this).val().length != 10 || isNaN($(this).val()))) {
                 $(this).css('border-color','#FF0000');
                 $("#phone_button").addClass('disabled');
+                $(this).removeClass('valid').addClass('invalid');
                 return false;
             } else {
                 $(this).css('border-color','#0aa827');
                 $("#phone_button").removeClass('disabled');
+                $(this).removeClass('invalid').addClass('valid');
             }
+        } else if (ids == "peru") {
+            $( "input[name=prefix-phone]" ).val( "+51" );
+            $("#sign-up-phone img").attr('src', "assets/img/Peru.png");
+            $("#sign-up-phone input").attr('placeholder', "123456789");
+            var prefijo = $("input[name=prefix-phone]").val();
 
+            if (prefijo == "+51" && ($(this).val().length != 9 || isNaN($(this).val()))) {
+                $(this).css('border-color','#FF0000');
+                $("#phone_button").addClass('disabled');
+                $(this).removeClass('valid').addClass('invalid');
+                return false;
+            } else {
+                $(this).css('border-color','#0aa827');
+                $("#phone_button").removeClass('disabled');
+                $(this).removeClass('invalid').addClass('valid');
+            }
+        }else if (ids == "chile") {
+            $( "input[name=prefix-phone]" ).val( "+56" );
+            $("#sign-up-phone img").attr('src', "assets/img/Chile.png");
+            $("#sign-up-phone input").attr('placeholder', "123456789");
         }
     });
 });
