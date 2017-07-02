7eda1d40d1fde0219aa16286f90d220b5a992a0e
VaneGarciaYara <vanegarciay@gmail.com>
2017-07-02 00:01:11 -0400

    validaciones en campos he iniciando refactor

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,18 +5,14 @@
         endingTop: '100%' // Ending top style attribute
     });
 
-    $("input[name=phone]").keyup(function(event) {
+    $("#sign-up-phone").on("keyup", "input[name=phone]", function(event) {
         var prefijo = $("input[name=prefix-phone]").val();
-
-        if (prefijo == "+56" && ($(this).val().length != 9 || isNaN($(this).val()))) {
-            $(this).css('border-color','#FF0000');
-            $("#phone_button").addClass('disabled');
-            $(this).removeClass('valid').addClass('invalid');
-            return false;
-        } else {
-            $(this).css('border-color','#0aa827');
-            $("#phone_button").removeClass('disabled');
-            $(this).removeClass('invalid').addClass('valid');
+        if (prefijo == "+56") {
+            validatePhone(9);
+        } else if (prefijo == "+51") {
+            validatePhone(9);
+        } else if (prefijo == "52") {
+            validatePhone(10);
         }
     });
 
@@ -26,38 +22,27 @@
             $( "input[name=prefix-phone]" ).val( "+52" );
             $("#sign-up-phone img").attr('src', "assets/img/Mexico.png");
             $("#sign-up-phone input").attr('placeholder', "1234567890");
-            var prefijo = $("input[name=prefix-phone]").val();
-
-            if (prefijo == "+52" && ($(this).val().length != 10 || isNaN($(this).val()))) {
-                $(this).css('border-color','#FF0000');
-                $("#phone_button").addClass('disabled');
-                $(this).removeClass('valid').addClass('invalid');
-                return false;
-            } else {
-                $(this).css('border-color','#0aa827');
-                $("#phone_button").removeClass('disabled');
-                $(this).removeClass('invalid').addClass('valid');
-            }
         } else if (ids == "peru") {
             $( "input[name=prefix-phone]" ).val( "+51" );
             $("#sign-up-phone img").attr('src', "assets/img/Peru.png");
             $("#sign-up-phone input").attr('placeholder', "123456789");
-            var prefijo = $("input[name=prefix-phone]").val();
-
-            if (prefijo == "+51" && ($(this).val().length != 9 || isNaN($(this).val()))) {
-                $(this).css('border-color','#FF0000');
-                $("#phone_button").addClass('disabled');
-                $(this).removeClass('valid').addClass('invalid');
-                return false;
-            } else {
-                $(this).css('border-color','#0aa827');
-                $("#phone_button").removeClass('disabled');
-                $(this).removeClass('invalid').addClass('valid');
-            }
         }else if (ids == "chile") {
             $( "input[name=prefix-phone]" ).val( "+56" );
             $("#sign-up-phone img").attr('src', "assets/img/Chile.png");
             $("#sign-up-phone input").attr('placeholder', "123456789");
         }
     });
+
+    function validatePhone(phone_size) {
+        if ($("input[name=phone]").val().length != phone_size || isNaN($("input[name=phone]").val())) {
+            $("input[name=phone]").css('border-color','#FF0000');
+            $("#phone_button").addClass('disabled');
+            $("input[name=phone]").removeClass('valid').addClass('invalid');
+            return false;
+        } else {
+            $("input[name=phone]").css('border-color','#0aa827');
+            $("#phone_button").removeClass('disabled');
+            $("input[name=phone]").removeClass('invalid').addClass('valid');
+        }
+    }
 });
