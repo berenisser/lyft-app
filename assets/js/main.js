1bd3e10d92ef4347659643f868ce7533d6296897
VaneGarciaYara <vanegarciay@gmail.com>
2017-07-01 20:11:33 -0400

    validacion en formulario y activacion del boton apenas cumple condicion

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,5 +4,20 @@
         startingTop: '4%', // Starting top style attribute
         endingTop: '100%' // Ending top style attribute
     });
+    $('.tooltipped').tooltip();
 
+    $("input[name=phone]").keyup(function(event) {
+        var prefijo = $("input[name=prefix-phone]").val();
+
+        if (prefijo == "+56" && ($(this).val().length != 9 || isNaN($(this).val()))) {
+            $(this).css('border-color','#FF0000');
+            $("#phone_button").addClass('disabled');
+            return false;
+        }
+        else {
+            $(this).css('border-color','#0aa827');
+            $("#phone_button").removeClass('disabled');
+            $('.tooltipped').tooltip('remove');
+        }
+    });
 });
