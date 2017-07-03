1ecb2ccb08007f0f16573547013e08fda6cc0713
Maribel Diaz <maribel.diaz.j@gmail.com>
2017-07-02 21:49:35 -0300

    cargando mapa

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,13 @@
-
-
-
-
+function initMap() {
+    
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 12,
+        center: { lat: -33.5157491, lng: -70.600865 },
+        mapTypeControl: false,
+        zoomControl: false,
+        streetViewControl: false
+    });
+    
+};
 
 
