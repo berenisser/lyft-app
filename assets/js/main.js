c86c78da198a613922dfe69d1b7d8bcd1972bf51
kJeriar <karla.jeria@gmail.com>
2017-07-03 21:56:18 -0400

    js condiciones de datos

--- assets/js/main.js
+++ assets/js/main.js
@@ -188,3 +188,36 @@ $(document).ready(function() {
     // FIN Seccion profile.html
     
 });
+
+    $(document).ready(function() {
+    $("button").click(function() {
+        first_name();
+        last_name();
+        email();
+    }); 
+
+
+function email(){
+var emailValue = $("#input-email").val();
+if (!(/^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/).test(emailValue)){
+$(".email").append('<span> formato de correo no valido</span>');
+$("#input-email").val("");
+}
+}
+
+function first_name(){
+    var nameValue = $("#first_name").val();
+            if (!(/^[A-Z][a-z]/).test(nameValue)){
+            $(".first_name").append('<span>Nombre inválido.</span>');
+            $("#first_name").val("");
+            }
+    }
+
+function last_name(){
+    var nameValue = $("#last_name").val();
+            if (!(/^[A-Z][a-z]/).test(nameValue)){
+            $(".last_name").append('<span>apellido inválido.</span>');
+            }
+    }
+});
+
